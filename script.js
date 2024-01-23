d3fd5bc7cefc34889e7fa976dc012ec4506df954
JGJT1 <jjales725@gmail.com>
2024-01-23 13:02:26 -0300

    bug resolvido

--- script.js
+++
@@ -1,9 +0,0 @@
-function ajustarPadding(){
-    var inburguer = document.getElementById('in-burguer')
-    var lista = document.getElementById('lista')
-    if(inburguer.checked){
-        lista.style.paddingBottom = '0px'
-    }else{
-        lista.style.paddingBottom = '0px'
-    }
-}
