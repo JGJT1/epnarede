bac10f6a72921892563b3abfb826c5d7d50348d9
JGJT1 <jjales725@gmail.com>
2024-01-27 17:10:47 -0300

    novo menu

--- versões teste/script.js
+++ versões teste/script.js
@@ -1,9 +1,40 @@
-function ajustarPadding(){
-    var inburguer = document.getElementById('in-burguer')
-    var lista = document.getElementById('lista')
-    if(inburguer.checked){
-        lista.style.paddingBottom = '0px'
-    }else{
-        lista.style.paddingBottom = '0px'
-    }
-}
+
+document.getElementById('burguer-btn').addEventListener('click', function(){
+document.getElementById("main-menu").style.width = '250px';
+});
+
+document.getElementById("escola-btn").addEventListener("click", function() {
+    document.getElementById("escola-submenu").style.width = "250px";
+});
+
+document.getElementById('cursos-btn').addEventListener('click', function(){
+    document.getElementById('cursos-submenu').style.width = '250px';
+});
+
+document.getElementById('fechar-menu').addEventListener('click', function(){
+    document.getElementById('main-menu').style.width = '0'
+    document.getElementById('escola-submenu').style.width = '0'
+    document.getElementById('cursos-submenu').style.width = '0'
+})
+
+document.getElementById('fechar-menu-escola').addEventListener('click', function(){
+    document.getElementById('escola-submenu').style.width = '0'
+    document.getElementById('cursos-submenu').style.width = '0'
+    document.getElementById('main-menu').style.width = '0'
+})
+
+document.getElementById('fechar-menu-cursos').addEventListener('click', function(){
+    document.getElementById('cursos-submenu').style.width = '0'
+    document.getElementById('escola-submenu').style.width = '0'
+    document.getElementById('main-menu').style.width = '0'
+})
+
+document.getElementById('voltar-escola').addEventListener('click', function(){
+    document.getElementById('escola-submenu').style.width = '0';
+    document.getElementById('main-menu').style.width = '250px'
+});
+
+document.getElementById('voltar-cursos').addEventListener('click', function(){
+    document.getElementById('cursos-submenu').style.width = '0';
+    document.getElementById('main-menu').style.width = '250px'
+});
