5a5bcfbe59fdbb80a80a35a14076b7b234c1e579
JGJT1 <jjales725@gmail.com>
2024-02-25 15:29:04 -0300

    otimização de js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var main = document.querySelector("main")
+var logoPag1 = document.querySelector("#logo")
+
+function fecharMenus(){
+    document.getElementById('main-menu').style.width = '0'
+    document.getElementById('escola-submenu').style.width = '0'
+    document.getElementById('cursos-submenu').style.width = '0'
+}
 
 document.getElementById('burguer-btn').addEventListener('click', function(){
 document.getElementById("main-menu").style.width = '250px';
@@ -11,23 +19,11 @@ document.getElementById('cursos-btn').addEventListener('click', function(){
     document.getElementById('cursos-submenu').style.width = '250px';
 });
 
-document.getElementById('fechar-menu').addEventListener('click', function(){
-    document.getElementById('main-menu').style.width = '0'
-    document.getElementById('escola-submenu').style.width = '0'
-    document.getElementById('cursos-submenu').style.width = '0'
-})
+document.getElementById('fechar-menu').addEventListener('click',fecharMenus);
 
-document.getElementById('fechar-menu-escola').addEventListener('click', function(){
-    document.getElementById('escola-submenu').style.width = '0'
-    document.getElementById('cursos-submenu').style.width = '0'
-    document.getElementById('main-menu').style.width = '0'
-})
+document.getElementById('fechar-menu-escola').addEventListener('click',fecharMenus);
 
-document.getElementById('fechar-menu-cursos').addEventListener('click', function(){
-    document.getElementById('cursos-submenu').style.width = '0'
-    document.getElementById('escola-submenu').style.width = '0'
-    document.getElementById('main-menu').style.width = '0'
-})
+document.getElementById('fechar-menu-cursos').addEventListener('click',fecharMenus);
 
 document.getElementById('voltar-escola').addEventListener('click', function(){
     document.getElementById('escola-submenu').style.width = '0';
@@ -39,10 +35,8 @@ document.getElementById('voltar-cursos').addEventListener('click', function(){
     document.getElementById('main-menu').style.width = '250px'
 });
 
-function mudouTam(){
-    if (window.innerWidth >= 768){
-        document.getElementById('main-menu').style.width = '0';
-        document.getElementById('escola-submenu').style.width = '0';
-        document.getElementById('cursos-submenu').style.width = '0';
-    }
-}
+function mudouTam(){if (window.innerWidth >= 768){fecharMenus();}}
+
+main.addEventListener('click',fecharMenus);
+
+logoPag1.addEventListener('click',fecharMenus);
